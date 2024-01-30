878f40862c2f0af985ba0e85d4df5443bae70e5d
anrwn <andrenrwn@users.noreply.github.com>
2024-01-31 07:06:03 +0800

    Add contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,38 @@
+import { useState } from "react";
+
 export default function Contact() {
+  // Set state variables for name, email and message using `useState`
+  const [guestname, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = e.target;
+
+    // return a set state value result based on input field
+    let res;
+    if (name === "guestname") {
+      res = setName(value);
+    } else if (name === "email") {
+      res = setEmail(value);
+    } else if (name === "message") {
+      res = setMessage(value);
+    }
+    return res;
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // Alert the user their first and last name, clear the inputs
+    alert(`Hello ${guestname}, your email is: ${email} and your message is: ${message}`);
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
   return (
     <div className="">
       <div>
@@ -6,7 +40,7 @@ export default function Contact() {
           <h1 className="text-5xl">Contact</h1>
         </aside>
         <section className="content m-2">
-          <article className="">
+          <article className="flex flex-wrap">
             <div className="m-2">
               <h2>Contact Me</h2>
               <p>
@@ -14,12 +48,69 @@ export default function Contact() {
               </p>
               <p>
                 E-mail:{" "}
-                <a href="mailto: 141073152+andrenrwn@users.noreply.github.com">
+                <a
+                  href={
+                    "mailto:141073152+andrenrwn@users.noreply.github.com?subject=Contact form&from=" +
+                    email +
+                    "&body=Hello, this is " +
+                    guestname +
+                    "" +
+                    message
+                  }
+                >
                   141073152+andrenrwn@users.noreply.github.com
                 </a>
               </p>
             </div>
-            <img src="./assets/images/photo_avatar.png" className="photoavatar" alt="my profile photo" target="_blank" />
+            <img
+              src="./assets/images/photo_avatar.png"
+              className="photoavatar"
+              alt="my profile photo"
+              target="_blank"
+            />
+            <div className="container">
+              <h1>Hello {guestname}, please enter your contact information:</h1>
+              <form className="form flex flex-col w-full justify-center" onSubmit={handleFormSubmit}>
+                <input
+                  className="input input-bordered input-info w-full"
+                  value={guestname}
+                  name="guestname"
+                  onChange={handleInputChange}
+                  type="text"
+                  placeholder="Name"
+                />
+                <input
+                  className="input input-bordered input-info w-full"
+                  value={email}
+                  name="email"
+                  onChange={handleInputChange}
+                  type="text"
+                  placeholder="email"
+                />
+                <input
+                  className="input input-bordered input-info w-full"
+                  value={message}
+                  name="message"
+                  onChange={handleInputChange}
+                  type="text"
+                  placeholder="type a message"
+                />
+                <a
+                  className="btn btn-primary"
+                  type="submit"
+                  href={
+                    "mailto:141073152+andrenrwn@users.noreply.github.com?subject=Contact form&from=" +
+                    email +
+                    "&body=Hello, this is " +
+                    guestname +
+                    "" +
+                    message
+                  }
+                >
+                  Send to 141073152+andrenrwn@users.noreply.github.com
+                </a>
+              </form>
+            </div>
           </article>
         </section>
       </div>
